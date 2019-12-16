595701d0cde37438dd0d093d3e1aebd3b3b97890
Pettroo <pioter780@wp.pl>
2019-12-16 23:47:22 +0100

    Flyway added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.DatabaseException;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
-import org.hibernate.service.Service;
-import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) throws SQLException, LiquibaseException {
+    public static void main(String[] args) {
         launch(args);
     }
 
